b0e03c332bad09847a52cb5e5ca141cba83536c0
danishmazhar <153164619+danishmazhar@users.noreply.github.com>
2024-01-04 20:33:22 +0500

    Add files via upload

---
+++ danish/main.py
@@ -0,0 +1,28 @@
+#Area of triangle:
+
+base = float(input("Enter the base of the triangle:"))
+height =float(input("Enter the height of the triangle:"))
+
+area =(base * height) / 2
+
+print("The area of triangle is:", area)
+
+#Average of two numbers:
+
+a=float(input("Enter the first number:"))
+b=float(input("Enter the second number"))
+
+average = (a + b) /2
+
+print("The average is" , average)
+
+#volume and surface area of a sphere:
+
+radius = float(input("Enter the radius of the sphere:"))
+
+volume = (4/3) * 3.142 * (radius **3)
+
+surface_area = 4 * 3.142 * (radius **2)
+
+print("The volume of the sphere is:", volume)
+
